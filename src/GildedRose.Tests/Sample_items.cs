7d6f31e1c31924d91ddd469464f5ceee89fba4e4
Andrew Pattison <49647893+andy-codurance@users.noreply.github.com>
2019-08-14 11:47:37 +0100

    tests: aged brie quality increases in quality

--- src/GildedRose.Tests/Sample_items.cs
+++ src/GildedRose.Tests/Sample_items.cs
@@ -102,4 +102,20 @@ namespace GildedRose.Tests
             item.Quality.Should().BeLessOrEqualTo(50);
         }
     }
+
+    public class Aged_Brie_characteristics
+    {
+        [Fact]
+        public void Quality_increase_by_1_as_sellin_decreases_by_1()
+        {
+            var item = new Item {Name = "Aged Brie", Quality = 10, SellIn = 10};
+
+            var app = ProgramFactory.Create(item);
+            
+            app.UpdateQuality();
+            
+            var actual = new {item.Quality, item.SellIn};
+            actual.Should().Be(new {Quality = 11, SellIn = 9});
+        }
+    }
 }
